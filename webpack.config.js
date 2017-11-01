2c3e2c2c081cbd62c07d78ca05c1f00c738c42f8
dung.ngo <dung.ngo@niteco.se>
2017-11-01 17:47:50 +0700

    Fix bug webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,55 +13,55 @@ module.exports = {
         app: [
             'babel-polyfill',
             path.resolve(__dirname, './src/js/main.js')
-        ],
-        devtool: 'source-map',
-        output: {
-            pathinfo:  true,
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: './dist/',
-            filename: 'main.js'
-        },
-        watch: true,
-        plugins: [
-            definePlugin,
-            new UglifyJsPlugin({
+        ]
+    },
+    devtool: 'source-map',
+    output: {
+        pathinfo:  true,
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: './dist/',
+        filename: 'main.js'
+    },
+    watch: true,
+    plugins: [
+        definePlugin,
+        new UglifyJsPlugin({
+            test: /\.js$/,
+            sourceMap: false,
+            parallel: true
+        })
+    ],
+    module: {
+        rules: [
+            {
                 test: /\.js$/,
-                sourceMap: false,
-                parallel: true
-            })
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    include: path.join(__dirname, 'src'),
-                    use: [{
-                        loader: 'babel-loader',
-                        options: {
-                          presets: [['es2015', {modules: false}]],
-                          plugins: ['syntax-dynamic-import']
-                        }
-                    }]
-                }, {
-                    test: /\.(css|scss)$/,
-                    use: ExtractTextPlugin.extract({
-                      fallback: 'style-loader',
-                      use: [{
-                          loader: "css-loader"
-                      },
-                      {
-                          loader: "sass-loader"
-                      }],
-                    }),
-                }, {
-                    test: /\.jade$/,
-                    use: {
-                        loader: "jade-loader"
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['es2015', {modules: false}]],
+                        plugins: ['syntax-dynamic-import']
                     }
+                }]
+            }, {
+                test: /\.(css|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    }],
+                }),
+            }, {
+                test: /\.jade$/,
+                use: {
+                    loader: "jade-loader"
                 }
-            ]
-        }
+            }
+        ]
     }
 }
 
